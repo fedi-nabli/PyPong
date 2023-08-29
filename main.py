7cbd5fd26add58f96f190dc336da532b48cbf7b0
fedi-nabli <fedinabli@gmail.com>
2023-08-30 00:20:09 +0100

    Added smash element

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ angles = [0, 1, 2]
 
 #COLORS
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 
@@ -30,6 +31,10 @@ left_paddle_y = right_paddle_y = HEIGHT/2 - paddle_height/2
 left_paddle_x, right_paddle_x = 100 - paddle_width/2, WIDTH - (100 - paddle_width/2)
 right_paddle_vel = left_paddle_vel = 0
 
+#GADGETS
+left_gadget = right_gadget = 0
+left_gadget_remaining = right_gadget_remaining = 5
+
 #main loop
 while run:
   window.fill(BLACK)
@@ -41,10 +46,14 @@ while run:
         right_paddle_vel = -0.9
       if event.key == pygame.K_DOWN:
         right_paddle_vel = 0.9
+      if event.key == pygame.K_RIGHT and right_gadget_remaining > 0:
+        right_gadget = 1
       if event.key == pygame.K_z:
         left_paddle_vel = -0.9
       if event.key == pygame.K_s:
         left_paddle_vel = 0.9
+      if event.key == pygame.K_d and left_gadget_remaining > 0:
+        left_gadget = 1
 
     if event.type == pygame.KEYUP:
       right_paddle_vel = 0
@@ -101,12 +110,6 @@ while run:
   if right_paddle_y <= 0:
     right_paddle_y = 0
 
-  #MOUVEMENT
-  ball_x += ball_vel_x
-  ball_y += ball_vel_y
-  right_paddle_y += right_paddle_vel
-  left_paddle_y += left_paddle_vel
-
   #collision
   #left
   if left_paddle_x <= ball_x <= left_paddle_x + paddle_width:
@@ -119,10 +122,37 @@ while run:
       ball_x = right_paddle_x
       ball_vel_x *= -1
 
+  #gadgets in action
+  if left_gadget == 1:
+    if left_paddle_x <= ball_x <= left_paddle_x + paddle_width:
+      if left_paddle_y <= ball_y <= left_paddle_y + paddle_height:
+        ball_x = left_paddle_x + paddle_width
+        ball_vel_x *= -3.5
+        left_gadget = 0
+        left_gadget_remaining -= 1
+  if right_gadget == 1:
+    if right_paddle_x <= ball_x <= right_paddle_x + paddle_width:
+      if right_paddle_y <= ball_y <= right_paddle_y + paddle_height:
+        ball_x = right_paddle_x
+        ball_vel_x *= -3.5
+        right_gadget = 0
+        right_gadget_remaining -= 1
+
+  #MOUVEMENT
+  ball_x += ball_vel_x
+  ball_y += ball_vel_y
+  right_paddle_y += right_paddle_vel
+  left_paddle_y += left_paddle_vel
+
   #OBJECTS
   pygame.draw.circle(window, BLUE, (ball_x, ball_y), radius)
   pygame.draw.rect(window, RED, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
   pygame.draw.rect(window, RED, pygame.Rect(right_paddle_x, right_paddle_y, paddle_width, paddle_height))
+
+  if left_gadget == 1:
+    pygame.draw.circle(window, WHITE, (left_paddle_x + 10, left_paddle_y + 10), 4)
+  if right_gadget == 1:
+    pygame.draw.circle(window, WHITE, (right_paddle_x + 10, right_paddle_y + 10), 4)
 
   pygame.display.update()
   #clock.tick(FPS)
